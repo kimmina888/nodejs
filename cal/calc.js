75b469069f188aab8e0b17f03b2440cde516ac2a
mina kim <81088157+kimmina888@users.noreply.github.com>
2022-03-15 23:06:15 +0900

    Add files via upload

---
+++ cal/calc.js
@@ -0,0 +1,15 @@
+"use strict";
+var calc = {};
+calc.add = function(a,b){
+    return a + b;
+}
+ calc.sub = function(a,b){
+    return a - b;
+}
+calc.mul = function(a,b){
+    return a * b;
+}
+calc.div = function(a,b){
+    return a / b;
+}
+module.export = calc;
