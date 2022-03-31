727073a798e39852a8071b5e105a767acd9dab32
mina kim <81088157+kimmina888@users.noreply.github.com>
2022-03-31 09:25:15 +0900

    1

--- ch05/app7.js
+++ ch05/app7.js
@@ -1,16 +1,25 @@
-// Express 기본 모듈 불러오기
-var express = require('express')
-, http = require('http')
-, path = require('path');
-// Express의 미들웨어 불러오기
-var bodyParser = require('body-parser')
-, static = require('serve-static');
-// 익스프레스 객체 생성
+var express = require('express');
+http = require('http');
+path = require('path');
+
+var bodyParser = require('body-parser');
+const { runInNewContext } = require('vm');
+static = require('serve-static');
 var app = express();
-// 기본 속성 설정
-app.set('port', process.env.PORT || 3000);
-// body-parser를 이용해 application/x-www-form-urlencoded 파싱
-app.use(bodyParser.urlencoded({ extended: false }))
-// body-parser를 이용해 application/json 파싱
-app.use(bodyParser.json())
-app.use('/public', static(path.join(__dirname, 'public')));
+
+app.set('port',process.env.PORT||3000);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/',static(path.join(__dirname,'public')));
+app.use(function(req,res,next){
+    console.log('첫번째 미들웨어에서 요청을 처리함.');
+
+    var paramId = req.body.id||req.query.id;
+    var paramPassword = req.body.password||req.query.password;
+
+    res.writeHead('200',{'Content-type':'text/html/charset=utf8'});
+    res.write('<h1>Express 서버에서 응답한 결과 입니다.</h1>');
+    res.write('<div><p>paramId : '+paramId+'</p></div>');
+    res.write('<div><p>paramPassword : '+paramPassword+'</p></div>');
+    res.end();
+})
